7e6fc1f6fff18685d55179d1dc687e121578c965
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-07-22 22:02:13 +0200

    key errors

--- generic.cpp
+++ generic.cpp
@@ -3,7 +3,8 @@
 #include <string>
 
 const std::string exec(const std::string cmd) {
-  char buffer[128];
+  const unsigned int buflen = 128;
+  char buffer[buflen];
   std::string result = "";
   #ifdef _WIN32
   FILE* pipe = _popen(cmd.c_str(), "r");
@@ -11,12 +12,12 @@ const std::string exec(const std::string cmd) {
   FILE* pipe = popen(cmd.c_str(), "r");
   #endif
   if (!pipe) {
-    printf("popen() failed!");
+    printf("popen() failed");
     return std::string("");
   }
   try {
     while (!feof(pipe)) {
-      if (fgets(buffer, 128, pipe) != NULL) result.append(buffer, 128);
+      if (fgets(buffer, buflen, pipe) != NULL) result.append(buffer, buflen);
     }
   } catch (...) {
     #ifdef _WIN32
@@ -24,7 +25,8 @@ const std::string exec(const std::string cmd) {
     #else
     pclose(pipe);
     #endif
-    throw;
+    printf("pipe failed");
+    return std::string("");
   }
   #ifdef _WIN32
   _pclose(pipe);
@@ -35,17 +37,18 @@ const std::string exec(const std::string cmd) {
 }
 
 int main(const int argc, const char* argv[]) {
-  if (argc < 3) {
+  const unsigned int ZERO = 0, ONE = 1, TWO = 2, THREE = 3, FOUR = 4, FIVE = 5;
+  if (argc < THREE) {
     printf("gimme at least 2 arguments\n");
-    return 1;
+    return ONE;
   }
   std::string s;
   s.append("R --vanilla --slave -e \"jsonmatch::jsonmatch('")
-   .append(argv[1]).append("','").append(argv[2]).append("'");
-  if (argc >= 4) s.append(",").append(argv[3]);
-  if (argc == 5) s.append(",").append(argv[4]);
+   .append(argv[ONE]).append("','").append(argv[TWO]).append("'");
+  if (argc >= FOUR) s.append(",").append(argv[THREE]);
+  if (argc == FIVE) s.append(",").append(argv[FOUR]);
   s.append(")\" 2>&1");
   const std::string rtn = exec(s);
   printf(rtn.c_str());
-  return s.compare("") ? 0 : 1;
+  return rtn.compare("") ? ZERO : ONE;
 }
